ddd5c6333e87236b1866c3043a9999f193a68ae9
FernandaRubio74 <20235795@esen.edu.sv>
2025-05-19 21:57:05 -0600

    client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule } from './clients/clients.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Client } from './clients/client.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type:'postgres',
+    //mis cosos
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '1234',
+    database: 'semana_3',
+    entities:[Client],
+    synchronize: true,
+  }), ClientsModule],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
