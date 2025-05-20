85d003903cb3ca81d1a6d6bf5db9d9401ddf8a2d
Nahomi Serrano <snahomimichelle@gmail.com>
2025-05-19 22:16:24 -0600

    no se que cambios habian

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ReservaController } from './reserva/reserva.controller';
-import { ReservaService } from './reserva/reserva.service';
-import { ReservaModule } from './reserva/reserva.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { reserva } from './reserva/reserva.entity';
+
+
 
 @Module({
-    imports: [TypeOrmModule.forRoot({
-        type: 'postgres',
-        host: process.env.DB_HOST,
-        port: 5432,
-        username: 'postgres',
-        password: 'suser',
-        database: 'semana_3',
-        // include the entities defined in the exercises
-        entities: [],
-        synchronize: true,
-    })],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'suser',
+      database: 'semana_3',
+      entities: [reserva],
+      synchronize: true,
+    }),
+  ],
 })
-export class AppModule { }
+export class AppModule {}
