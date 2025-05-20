d785e707f0be288834e75cab755d75e17f4f5dea
NatalyPonce <20235782@esen.edu.sv>
2025-05-19 19:51:05 -0600

    creo que funciona

--- src/producto/producto.controller.ts
+++ src/producto/producto.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body  } from '@nestjs/common';
+import { ProductoService } from './producto.service';
+
 
 @Controller('producto')
-export class ProductoController {}
+export class ProductoController {
+    constructor(private readonly productoService: ProductoService) {}
+
+    @Post()
+    create(@Body() body: { id: number; nombre: string; precio: number; stock: number }) {
+        if(!body.id || !body.nombre || !body.precio || !body.stock){
+            return { error: 'Faltan datos en el body' };
+        }
+        if(body.precio < 0 || body.stock < 0){
+            return { error: 'El precio y el stock deben ser mayores a 0' };
+        }
+        const { id, nombre, precio, stock } = body;
+        return this.productoService.createProduct(id, nombre, precio, stock);
+    }
+}
--- src/producto/producto.service.ts
+++ src/producto/producto.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProductoService {}
+export class ProductoService {
+    constructor(
+        @InjectRepository(Producto)
+        private productoRepository: Repository<Producto>,
+    ) {}
+
+    async createProduct( id: number, nombre: string, precio: number, stock: number): Promise<Producto> {
+        const newProduct = this.productoRepository.create({ id, nombre, precio, stock });
+        return this.productoRepository.save(newProduct);
+    }
+
+}
