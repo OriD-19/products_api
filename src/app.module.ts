2d8fec9e8552617fe37ddd3d29e6fd651bdf11ae
Nahomi Serrano <snahomimichelle@gmail.com>
2025-05-19 19:30:50 -0600

    prueba

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReservaController } from './reserva/reserva.controller';
+import { ReservaService } from './reserva/reserva.service';
+import { ReservaModule } from './reserva/reserva.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: 5432,
+        username: 'postgres',
+        password: 'suser',
+        database: 'semana_3',
+        // include the entities defined in the exercises
+        entities: [],
+        synchronize: true,
+    })],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
